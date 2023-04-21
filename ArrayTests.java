d30644c62e7ee55f5a38c0b5d28692e5bcffa311
nlormsby <nlormsby@gmail.com>
2023-04-20 18:58:09 -0700

    ok

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,4 +1,7 @@
 import static org.junit.Assert.*;
+
+import java.beans.Transient;
+
 import org.junit.*;
 
 public class ArrayTests {
@@ -14,5 +17,20 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+
+  @Test
+  public void testRIPSecond() {
+    int[] input1 = {1, 2, 3, 4, 5};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{5, 4, 3, 2, 1}, input1);
+  }
+
+  @Test
+  public void testRevSecond() {
+    int[] input1 = {1, 2, 3, 4, 5};
+    assertArrayEquals(new int[]{5, 4, 3, 2, 1}, 
+    ArrayExamples.reversed(input1));
   }
 }
